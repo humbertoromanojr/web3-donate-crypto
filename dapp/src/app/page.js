998b4176d2158e967cb1c416a6828feee6c4cb6c
Humberto Junior <astronomi@gmail.com>
2025-09-08 20:36:29 -0300

    Updated html

--- dapp/src/app/page.js
+++ dapp/src/app/page.js
@@ -24,7 +24,27 @@ export default function Home() {
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-2">&nbsp;</div>
           <div className="col-8 mt-5">
-            <img src="/link-shield.png" alt="" />
+            <h1 className="display-5 fw-bold lh-1 mb-3 text-body-emphasis">
+              Donate Crypto
+            </h1>
+            <p className="lead">Sua plataforma descentralizada de Doações.</p>
+            <p className="lead">
+              Autentique-se com sua carteira e crie sua campanha.
+            </p>
+            <p className="lead mb-4">
+              Para Doações, use o link da campanha já existente.
+            </p>
+            <div className="d-flex justify-content-start mt-5">
+              <button className="btn btn-primary btn-lg px-4 me-2">
+                <img
+                  src="/logo-metamask.png"
+                  alt="logo metamask"
+                  width={32}
+                  className="me-2"
+                />
+                Conectar com a Metamask
+              </button>
+            </div>
           </div>
           <div className="col-2">&nbsp;</div>
         </div>
