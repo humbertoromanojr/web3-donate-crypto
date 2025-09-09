4c6f11ce879fdf71bc3d935f99fec703cee30040
Humberto Junior <astronomi@gmail.com>
2025-09-08 21:37:55 -0300

    Updated text and html

--- dapp/src/app/create/page.js
+++ dapp/src/app/create/page.js
@@ -1,11 +1,12 @@
 "use client";
 
-export default function Home() {
+export default function Create() {
   const backgroundImageStyle = {
     backgroundImage: "url(/people-sunset.jpg)",
     backgroundSize: "cover",
     backgroundPosition: "center",
-    height: "80vh",
+    height: "100vh",
+    width: "100vw",
     color: "white",
     margin: 0,
     padding: 0,
@@ -27,6 +28,54 @@ export default function Home() {
             <h1 className="display-5 fw-bold lh-1 mb-3 text-body-emphasis">
               Donate Crypto
             </h1>
+            <p>Fill in the fields to include your campaign on the platform.</p>
+            <p>
+              Once you have completed the registration process, you will receive
+              a link to promote your campaign and receive donations.
+            </p>
+            <hr className="mb-4" />
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                id="title"
+                className="form-control opacity-75"
+              />
+              <label htmlFor="title">Title</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <textarea id="description" className="form-control opacity-75" />
+              <label htmlFor="description">Description</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                id="imageUrl"
+                className="form-control opacity-75"
+              />
+              <label htmlFor="imageUrl">Imagem URL </label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                id="videoUrl"
+                className="form-control opacity-75"
+              />
+              <label htmlFor="videoUrl">Video URL</label>
+            </div>
+            <div className="mb-5">
+              <button
+                type="button"
+                className="btn btn-primary col-12 p-3 fw-bold mb-3 text-white"
+              >
+                Save
+              </button>
+            </div>
+            <div className="alert alert-success p-3 col-12 opacity-50">
+              Campaign successfully registered
+            </div>
           </div>
           <div className="col-2">&nbsp;</div>
         </div>
